a82c647af74bd4251365fc3b4668a72939c4af34
hamdon <hamdongunner@gmail.com>
2018-02-16 19:34:25 +0300

    final

--- resources/views/layout/dashmaster.blade.php
+++ resources/views/layout/dashmaster.blade.php
@@ -21,7 +21,7 @@
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet"/>
     <!--  Material Dashboard CSS    -->
     <link href="/assets/css/material-dashboard.css?v=1.2.0" rel="stylesheet"/>
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <!--  CSS for Demo Purpose, dont include it in your project     -->
     <link href="/assets/css/demo.css" rel="stylesheet"/>
     <!--     Fonts and icons     -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
